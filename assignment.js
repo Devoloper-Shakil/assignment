c10a3c6dde73d7c95c566bdde7cc4809568b930e
shakil hasan <shshakilhasan69@gmail.com.com>
2021-01-21 10:31:50 -0800

    first commit

---
+++ assignment.js
@@ -0,0 +1,53 @@
+// kiloMeterToMeter function
+function kiloMeterToMeter(kilometer){
+    var meter= kilometer*1000;
+    return meter;
+}
+ kiloMeterToMeter(6);
+
+ // budgetCalculator function
+
+ function budgeetCalculator(clock,phone,laptop){
+     var clocks = clock*50;
+     var phones = phone *100;
+     var laptops =laptop *500;
+     var trotal =clocks+phones+laptops;
+     return trotal;
+ }
+ budgeetCalculator(2,3,2);
+
+ // hotelCost function
+ function hotelCost (hotelday){
+     var cost = 0;
+     if(hotelday<=10){
+         cost=hotelday*100;
+     }
+     else if (hotelday<=20){
+         var fastPart=10*100;
+         var remaining =hotelday-10;
+         var secendPart=hotelday*80;
+         cost =fastPart+secendPart;
+     }
+     else {
+        var fastPart=10*100;
+        var secendPart = 10*80;
+        var remaining =hotelday-20;
+        var tradePart=hotelday*100;
+        cost =fastPart+secendPart+tradePart;
+
+     }
+     return cost;
+    }
+hotelCost(25);
+
+//megaFriend function
+function megaFriend(friend) {
+    let word = "";
+    for (let i = 0; i < friend.length; i++) {
+      if (word.length < friend[i].length) {
+        word = friend[i];
+      }
+    }
+    return word;
+  }
+ megaFriend(["shakil","sakil","himu","bangladesh"]);
